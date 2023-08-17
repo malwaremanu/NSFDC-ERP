7e5dc90b1aec3d557e6c18643fc3524e5b8c61e2
Manupal Choudhary <tech.manujpr@gmail.com>
2023-08-17 07:58:16 +0000

    HEADER in nuxt

---
+++ FRONTEND/my-project/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Header />
+
+    <div>
+      This is sample content
+    </div>
+
+  </div>
+</template>
---
+++ FRONTEND/my-project/pages/logout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div>
+
+
+        </div>
+
+        <center>
+            <div class="p-5">
+                <img src="~/assets/images/logo.png" />
+            </div>
+
+            <div class="text-2xl bg-green-600 text-white p-3">
+                You have been logged out successfully.
+            </div>
+
+            <div class="pt-5">
+                You will be redirected to login page in 5 seconds...
+            </div>
+
+
+        </center>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            v : 5
+        }
+    },
+    
+}
+</script>
