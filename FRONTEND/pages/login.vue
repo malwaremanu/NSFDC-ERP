96681937e790a8bc5790eaf881578ea0e6b0ddf1
Manupal Choudhary <tech.manujpr@gmail.com>
2024-05-15 15:49:00 +0530

    Updated LInk

--- FRONTEND/pages/login.vue
+++ FRONTEND/pages/login.vue
@@ -26,18 +26,20 @@
               <img class="hidden" src="~assets/images/loading.gif" />
 
               <div class="mt-10 text-center flex flex-col gap-2 items-center">
-                <a href="https://auth0.manupal.dev/c/ccvkwpl41wm1?r=https://erp.nsfdcdigital.in/auth"                  
+                <a :href="line"                  
                   class="px-4 py-2 bg-n-700 m-2 text-white rounded-full select-none cursor-pointer animate-bounce">
                   Login
                 </a>             
 
+                <span>{{  line  }}</span>
+
                 <div class="flex items-center gap-2">
-                  <a target="_blank" href="http://l.bankmill.in:8548/rgda/index.html?organization=636a392e5a1863baa1716512" >
+                  <a target="_blank" href="http://l.bankmill.in:8548/rgda/index.html?organization=636a392e5a1863baa1716512&usertype=Employee" >
                   <button class="w-full px-4 py-2 bg-green-700 m-2 text-white rounded-full select-none cursor-pointer" 
                   type="button"> Bankmill 1 </button>                  
                   </a>
 
-                  <a target="_blank" href="http://l2.bankmill.in:8548/rgda/index.html?organization=636a392e5a1863baa1716512" >
+                  <a target="_blank" class="hidden" href="http://l2.bankmill.in:8548/rgda/index.html?organization=636a392e5a1863baa1716512" >
                     <button class="w-full px-4 py-2 bg-green-700 m-2 text-white rounded-full select-none cursor-pointer" 
                     type="button"> Bankmill 2 </button>                  
                   </a>
@@ -87,12 +89,27 @@ export default {
       imageURL : 'https://nsfdc.nic.in/assets/images/magazine/carousel/1.jpg',
       currentImageIndex : 1,
       totalImages : 6,
+      auth_path : '',
+      // line : 'https://auth0.manupal.dev/c/ccvkwpl41wm1?r=https://erp.nsfdcdigital.in/auth'
+      line : 'checking'
     }
   },
   middleware: "authenticated",
   mounted() {
     console.log("Initiated.");
     setInterval(this.changeImageURL, 5000); 
+
+    // Getting the current URL
+    const currentURL = window.location.href;
+
+    // Replacing 'login' with 'auth' in the URL
+    const modifiedURL = currentURL.replace('/login', '/auth');
+    
+    // Assigning the modified URL to the data property
+    this.auth_path = modifiedURL;
+
+    this.line = this.$store.state.auth_domain + '/c/' + this.$store.state.org_id + '?r=' + modifiedURL
+
   },
   methods: {
     login() {
--- FRONTEND/store/index.js
+++ FRONTEND/store/index.js
@@ -1,6 +1,8 @@
 // store/index.js
 export const state = () => ({
     user: null,
+    org_id : 'ccvkwpl41wm1',
+    auth_domain : 'https://auth0.manupal.dev',
   })
   
   export const mutations = {
